a80c2d50fd18851fb4346b7bc14e8e1a054d047d
wuyibo <wuyibo@cmcm.com>
2018-04-30 00:05:31 +0800

    solved jump game

---
+++ src/jump_game_55.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        i = len(nums) - 2
+        start = i + 1
+        while i >= 0:
+            if start - i <= nums[i]:
+                start = i
+                i = start - 1
+            else:
+                i -= 1
+        return True if i + 1 == start else False
+
+
+if __name__ == '__main__':
+    o = Solution()
+    nums = [2, 3, 1, 1, 4]
+    # nums = [3, 2, 1, 0, 4]
+    ret = o.canJump(nums)
+    print(ret)
