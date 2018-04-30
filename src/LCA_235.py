13ce9167f045bb4ab0958b2361ddb217670340b5
wuyibo <wuyibo@cmcm.com>
2018-04-30 22:36:30 +0800

    solved symmetric tree

--- src/LCA_235.py
+++ src/LCA_235.py
@@ -15,30 +15,29 @@ class Solution(object):
         :rtype: TreeNode
         """
         import copy
-        # nums will be treated a stack
-        nums = []
+        stack = []
         ancestor_p = []
         ancestor_q = []
         last_visited = None
         found_p, found_q = False, False
 
-        while root is not None or len(nums) != 0:
+        while root is not None or len(stack) != 0:
             if root is not None:
-                nums.append(root)
+                stack.append(root)
                 root = root.left
-            elif len(nums) != 0:
-                tmp = nums[-1]
+            elif len(stack) != 0:
+                tmp = stack[-1]
                 if tmp.right is not None and tmp.right != last_visited:
                     root = tmp.right
                 else:
-                    tmp = nums.pop()
+                    tmp = stack.pop()
                     last_visited = tmp
                     if tmp == p:
-                        ancestor_p = copy.copy(nums)
+                        ancestor_p = copy.copy(stack)
                         ancestor_p.append(tmp)
                         found_p = True
                     if tmp == q:
-                        ancestor_q = copy.copy(nums)
+                        ancestor_q = copy.copy(stack)
                         ancestor_q.append(tmp)
                         found_q = True
                     if found_p and found_q:
---
+++ src/symmetric_tree_101.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is not None:
+            root.right = self.swap_tree(root.right)
+            return self.is_same_tree(root.left, root.right)
+        else:
+            return True
+
+    def swap_tree(self, root):
+        if root is not None:
+            left = self.swap_tree(root.left)
+            right = self.swap_tree(root.right)
+            root.left = right
+            root.right = left
+            return root
+        else:
+            return None
+
+    def is_same_tree(self, root1, root2):
+        if root1 is None:
+            if root2 is None:
+                return True
+            else:
+                return False
+        else:
+            if root2 is None:
+                return False
+            else:
+                return root1.val == root2.val and \
+                       self.is_same_tree(root1.left, root2.left) and \
+                       self.is_same_tree(root1.right, root2.right)
