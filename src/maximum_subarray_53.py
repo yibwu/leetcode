64491550c2690bafb542fb747c0b0a2e56e42e65
wuyibo <wuyibo@cmcm.com>
2018-05-02 22:24:05 +0800

    solved maximum subarray

---
+++ src/maximum_subarray_53.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_val = 0
+        tmp_sum = 0
+
+        for n in nums:
+            tmp_sum += n
+            if tmp_sum > 0:
+                max_val = tmp_sum if tmp_sum > max_val else max_val
+            else:
+                tmp_sum = 0
+        return max_val if max(nums) > 0 else max(nums)
+
+
+if __name__ == '__main__':
+    o = Solution()
+    nums = [-2,1,-3,4,-1,2,1,-5,4]
+    # nums = [-1]
+    ret = o.maxSubArray(nums)
+    print(ret)
