9ec283f85505d3e43d4b72bed7589db0bd0e2088
wuyibo <wuyibo@cmcm.com>
2018-05-08 21:01:00 +0800

    solved valid sodoku

---
+++ src/valid_sudoku_36.py
@@ -0,0 +1,85 @@
+class Solution:
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        return self.check_row(board) and \
+               self.check_column(board) and \
+               self.check_sub_box(board)
+
+    def check_row(self, board):
+        nums = set()
+        for row in board:
+            for val in row:
+                if val != '.':
+                    if val not in nums:
+                        nums.add(val)
+                    else:
+                        return False
+                else:
+                    pass
+            nums = set()
+        return True
+
+    def check_column(self, board):
+        nums = set()
+        for col in range(9):
+            for row in range(9):
+                val = board[row][col]
+                if val != '.':
+                    if val not in nums:
+                        nums.add(val)
+                    else:
+                        return False
+                else:
+                    pass
+            nums = set()
+        return True
+
+    def check_sub_box(self, board):
+        for row in range(0, 9, 3):
+            for col in range(0, 9, 3):
+                nums = set()
+                for i in range(0, 3):
+                    for j in range(0, 3):
+                        val = board[row + i][col + j]
+                        if val != '.':
+                            if val not in nums:
+                                nums.add(val)
+                            else:
+                                return False
+                        else:
+                            pass
+        return True
+
+
+if __name__ == '__main__':
+    o = Solution()
+    board = \
+    [
+        ["5","3",".",".","7",".",".",".","."],
+        ["6",".",".","1","9","5",".",".","."],
+        [".","9","8",".",".",".",".","6","."],
+        ["8",".",".",".","6",".",".",".","3"],
+        ["4",".",".","8",".","3",".",".","1"],
+        ["7",".",".",".","2",".",".",".","6"],
+        [".","6",".",".",".",".","2","8","."],
+        [".",".",".","4","1","9",".",".","5"],
+        [".",".",".",".","8",".",".","7","9"]
+    ]
+    # board = \
+    # [
+    #     ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    #     ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    #     [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    #     ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    #     ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    #     ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    #     [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    #     [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    #     [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+    # ]
+    ret = o.isValidSudoku(board)
+    print(ret)
+
