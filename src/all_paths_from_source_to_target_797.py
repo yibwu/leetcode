31c0c44014f1725f00b7590ff85ea0d3e198ae39
wuyibo <wuyibo@cmcm.com>
2018-06-12 11:00:18 +0800

    solved all paths from source to target

---
+++ src/all_paths_from_source_to_target_797.py
@@ -0,0 +1,39 @@
+class Solution:
+    def allPathsSourceTarget(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        degree = self.get_degree(graph)
+        v = [k for k, v in enumerate(degree) if v == 0]
+        result = []
+        tmp = []
+        for i in v:
+            self.visited(i, tmp, result, graph)
+        return result
+
+    def get_zero_degree(self, degree):
+        return [k for k, v in enumerate(degree) if v == 0]
+
+    def visited(self, i, tmp, result, graph):
+        tmp.append(i)
+        if i == len(graph) - 1:
+            import copy
+            result.append(copy.deepcopy(tmp))
+        for v in graph[i]:
+            self.visited(v, tmp, result, graph)
+        tmp.remove(i)
+
+    def get_degree(self, graph):
+        d = {i: 0 for i in range(len(graph))}
+        for edge in graph:
+            for v in edge:
+                d[v] = d.get(v, 0) + 1
+        return d
+
+
+if __name__ == '__main__':
+    graph = [[4,3,1],[3,2,4],[3],[4],[]]
+    o = Solution()
+    ret = o.allPathsSourceTarget(graph)
+    print(ret)
