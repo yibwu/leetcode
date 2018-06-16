df140550f05cc14b13271ac0711c978eb191bece
wuyibo <wuyibo@cmcm.com>
2018-06-17 01:45:15 +0800

    solved custom sort string

---
+++ src/custom_sort_string_791.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def customSortString(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: str
+        """
+        result = ''
+        ch_count = {}
+
+        for ch in T:
+            if ch in S:
+                ch_count[ch] = ch_count.get(ch, 0) + 1
+            else:
+                result += ch
+
+        for ch in S:
+            if ch in ch_count:
+                result += ch * ch_count[ch]
+
+        return result
+
+
+if __name__ == '__main__':
+    S = "kqep"
+    T = "pekeq"
+    o = Solution()
+    ret = o.customSortString(S, T)
+    print(ret)
