1f6583a1edc36c905bec3a25e589958998cd9af6
yibwu <wuyibo7788@163.com>
2018-07-14 01:26:04 +0800

    sovled pow

---
+++ src/pow_50.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        negative = True if n < 0 else False
+        n = abs(n) if n < 0 else n
+        result = self.fast_pow(x, n)
+        return 1.0 / result if negative else result
+
+        
+    def fast_pow(self, base, n):
+        if n == 0:
+            return 1
+        elif n % 2 == 0:
+            t = self.fast_pow(base, n // 2)
+            return t * t
+        else:
+            return base * self.fast_pow(base, n - 1)
+
+
+if __name__ == '__main__':
+    o = Solution()
+    base, n = 2.00000, 10
+    base, n = 2.10000, 3
+    base, n = 2.00000, -2
+    ret = o.myPow(base, n)
+    print(ret)
