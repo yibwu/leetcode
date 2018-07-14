ed7b4df077054163978e4921d3537a4df825f102
yibwu <wuyibo7788@163.com>
2018-07-14 17:11:19 +0800

    solved flipping an image

---
+++ src/flipping_an_image_832.py
@@ -0,0 +1,23 @@
+class Solution:
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        img = self.reverse_arr(A)
+        result = self.flip_arr(img)
+        return result
+
+    def reverse_arr(self, arr2d):
+        return [arr[::-1] for arr in arr2d]
+        
+    def flip_arr(self, arr2d):
+        return [list(map(lambda x: x ^ 1, arr)) for arr in arr2d]
+            
+
+if __name__ == '__main__':
+    o = Solution()
+    # img = [[1, 1, 0], [1, 0, 1], [0, 0, 0]] 
+    img = [[1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 0]]
+    ret = o.flipAndInvertImage(img)
+    print(ret)
