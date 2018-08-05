10dd9539f66c4fc7f754ebb469ca4ad536322b20
wuyibo <wuyibo@cmcm.com>
2018-08-05 13:17:42 +0800

    sovled letter combinations of a phone number

---
+++ src/letter_combinations_of_a_phone_number_17.py
@@ -0,0 +1,43 @@
+class Solution:
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+
+        TABLE = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+        letters = [TABLE[d] for d in digits]
+        result = []
+        self.helper(letters, 0, result)
+        return list(filter(lambda x: len(x) == len(letters), result))
+
+    def helper(self, letters, index, result):
+        if index == len(letters) - 1:
+            for ch in letters[index]:
+                result.append(ch)
+            return
+        else:
+            self.helper(letters, index + 1, result)
+            result_n_1 = list(filter(lambda x: len(x) == len(letters) - index - 1, result))
+            for ch in letters[index]:
+                for item in result_n_1:
+                    result.append(ch + item)
+            return
+
+
+if __name__ == '__main__':
+    o = Solution()
+    digits = '23'
+    ret = o.letterCombinations(digits)
+    print(ret)
