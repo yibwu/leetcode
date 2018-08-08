12ae9ab34ebf10fb5b40625a83ea5c0655e086b2
wuyibo <wuyibo@cmcm.com>
2018-08-08 22:56:48 +0800

    sovled position of large groups

---
+++ src/positions_of_large_groups_830.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def largeGroupPositions(self, S):
+        """
+        :type S: str
+        :rtype: List[List[int]]
+        """
+        result = []
+        if len(S) >= 3:
+            i, j = 0, 1
+            while i < len(S) - 1:
+                while j < len(S) and S[i] == S[j]:
+                    j += 1
+                if j - i >= 3:
+                    result.append([i, j - 1])
+                i = j
+                j = i + 1
+        return result
+
+
+if __name__ == '__main__':
+    o = Solution()
+    s = "abbxxxxzzy"
+    # s = "abcdddeeeeaabbbcd"
+    ret = o.largeGroupPositions(s)
+    print(ret)
