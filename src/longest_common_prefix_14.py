63446bc814869d709b99f3918e7956a54cf15bde
wuyibo <wuyibo@cmcm.com>
2018-08-08 23:14:01 +0800

    sovled longest common prefix

---
+++ src/longest_common_prefix_14.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            result = ''
+        else:
+            lens = [len(s) for s in strs]
+            len_min = min(lens)
+            i = 0
+            while i < len_min:
+                prefixs = [s[:i + 1] for s in strs]
+                if prefixs.count(strs[0][:i + 1]) != len(prefixs):
+                    break
+                else:
+                    i += 1
+            result = strs[0][:i]
+        return result
+
+
+if __name__ == '__main__':
+    o = Solution()
+    strs = ["flower", "flow", "flight"]
+    ret = o.longestCommonPrefix(strs)
+    print(ret)
