7d687cd617aa7b94f7726dbef935beb09cacb2f7
wuyibo <wuyibo@cmcm.com>
2018-08-29 00:34:28 +0800

    solved linked list cycle

---
+++ src/linked_list_cycle_141.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x, next):
+        self.val = x
+        self.next = next
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None or head.next is None:
+            return False
+        if head == head.next:
+            return True
+
+        slow, fast = head, head
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if not fast or slow == fast:
+                break
+        return True if slow == fast else False
+
+
+if __name__ == '__main__':
+    o = Solution()
+    head = ListNode(1, ListNode(2, None))
+    ret = o.hasCycle(head)
+    print(ret)
