c004b96759b3a11d6b17b39b4e11a79281efc4f6
wuyibo <wuyibo@p1.com>
2019-06-02 23:16:02 +0800

    rename variable

--- src/design_twitter_355.py
+++ src/design_twitter_355.py
@@ -37,24 +37,24 @@ if __name__ == '__main__':
     twitter.post_tweet(1, 5)
     
     # User 1's news feed should return a list with 1 tweet id -> [5].
-    res = twitter.get_news_feed(1)
-    print(res)
+    feed = twitter.get_news_feed(1)
+    print(feed)
     
     # User 1 follows user 2.
     twitter.follow(1, 2)
     
     # User 2 posts a new tweet (id = 6).
-    res = twitter.post_tweet(2, 6)
+    twitter.post_tweet(2, 6)
     
     # User 1's news feed should return a list with 2 tweet ids -> [6, 5].
     # Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
-    res = twitter.get_news_feed(1);
-    print(res)
+    feed = twitter.get_news_feed(1);
+    print(feed)
     
     # User 1 unfollows user 2.
     twitter.unfollow(1, 2);
     
     # User 1's news feed should return a list with 1 tweet id -> [5],
     # since user 1 is no longer following user 2.
-    res = twitter.get_news_feed(1)
-    print(res)
+    feed = twitter.get_news_feed(1)
+    print(feed)
