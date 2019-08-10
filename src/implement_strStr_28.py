6450aaf59f43d2a3a7508ec6cf10b6c133b5b781
Yibo Wu <wuyibo7788@163.com>
2019-08-10 17:05:51 +0800

    pass all cases in leetcode

--- src/implement_strStr_28.py
+++ src/implement_strStr_28.py
@@ -1,4 +1,4 @@
-def get_next(pattern):
+def get_next_table(pattern):
     next = [-1, 0]
     idx = 0
     for i in range(1, len(pattern) - 1):
@@ -6,33 +6,45 @@ def get_next(pattern):
             idx += 1
             next.append(next[-1] + 1)
         else:
-            if pattern[0] == pattern[i]:
-                idx = 1
-                next.append(1)
-            else:
-                idx = 0
-                next.append(0)
+			if pattern[0] == pattern[i]:
+				next.append(idx)
+			else:
+				idx = 0
+				next.append(0) 
     return next
 
 
 def match(s, pattern):
-    next = get_next(pattern)
+    if not pattern:
+        return 0 
+    if not s:
+        return -1   
+
+    next = get_next_table(pattern)
     i, j = 0, 0
     while i < len(s) and j < len(pattern):
         if s[i] == pattern[j]:
             i += 1
             j += 1
         else:
-            if next[j] == -1:
+            j = next[j]
+            if j == -1:
                 i += 1
                 j += 1
-            else:
-                j = next[j]
-    return i - j if j == len(pattern) else -1
+    return i - j if j == len(pattern) and s[i - j] == pattern[0] else -1
         
     
 if __name__ == '__main__':
-    s = 'acabaabaabcaccaabc'
-    pattern = 'abaabcac'
-    res = match(s, pattern)
-    print(res)
+    cases = [
+        ('acabaabaabcaccaabc', 'abaabcac'),
+        ('hello', 'll'),
+        ('a', 'b'),
+        ('a', ''),
+        ('', ''),
+        ('', 'a'),
+        ('aabaaabaaac', 'aabaaac'),
+        ('aaaaabbbbb', 'bbbb'),
+    ]
+    for c in cases:
+        res = match(c[0], c[1])
+        print(res)
