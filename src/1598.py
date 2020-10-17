3aee684e6690c6601a35baf40b2b1c50b1f518cb
yibwu <wuyibo7788@163.com>
2020-10-17 23:37:02 +0800

    feat: Crawler Log Folder

---
+++ src/1598.py
@@ -0,0 +1,13 @@
+class Solution:
+    
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        for l in logs:
+            if l == "../":
+                if stack:
+                    stack.pop()
+            elif l == "./":
+                pass
+            else:
+                stack.append(l)
+        return len(stack)
